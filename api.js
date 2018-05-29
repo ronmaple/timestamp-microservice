5ba91b05a9615e3188c19f9a4337bb0562767ff6
Glitch (past-crib) <none>
2018-05-29 07:49:37 +0000

    🐾🍔 Checkpoint ./api.js:1216088/208

--- api.js
+++ api.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/home', (req, res) => {
-  res.send('Hello World!');
+app.get('views/home', (req, res) => {
+  app.use(express.static('public'));
+  res.sendFile(__dirname + 'views/home')
+  res.end('Hello World!');
 });
 
 app.listen(process.env.PORT);
