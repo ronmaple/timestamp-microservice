3a595ca5387add77b79f2c754a35f7aa686ec315
Glitch (ronmap-timestamp-restful-api) <none>
2018-05-29 08:00:42 +0000

    🐜💠 Checkpoint ./api.js:1216088/152

--- api.js
+++ api.js
@@ -2,10 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('views/home', (req, res) => {
-  app.use(express.static('public'));
-  res.sendFile(__dirname + 'views/home')
-  res.end('Hello World!');
+app.route('/home', (req, res) => {
+  res.send('home');
 });
 
 app.listen(process.env.PORT);
