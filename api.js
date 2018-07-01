1dc219304dce70d281662c61a9d8f6be29f7adcb
Glitch (ronmap-timestamp-restful-api) <none>
2018-07-01 21:21:33 +0000

    🏃🎪 Checkpoint ./api.js:1216088/255 ./package.json:1216088/18

--- api.js
+++ api.js
@@ -6,29 +6,25 @@ const toNatural = require('./toNatural.js');
 
 const app = express();
 
-// app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname));
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, 'views/index.html'));
-  //process.cwd() + '/views/index.html'
 });
 
-app.get("/:time", (req, res) => {
+app.get("/api/timestamp/:time", (req, res) => {
   console.log('accessed');
   let rawTime = req.params.time;
   let convertedTime;
-  
-  console.log('rawTime', rawTime);
-  // isNum => check if all values are numeric string
+
   let isNum = /^\d+$/.test(rawTime);
   console.log('isNum', isNum);
   
   if (isNum) {
     console.log('isNum');
-    // convertedTime = toNatural(rawTime);
-    // console.log(convertedTime);
-    // res.send(convertedTime);
+    convertedTime = toNatural(rawTime);
+    console.log(convertedTime);
+    res.send(convertedTime);
   } else {
     console.log('else');
     convertedTime = toUnix(rawTime);
