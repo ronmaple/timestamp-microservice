ecb4e53b973b53f96a1fcb083170e490635c62ca
Glitch (ronmap-timestamp-restful-api) <none>
2018-07-01 18:43:56 +0000

    🚥🗃 Checkpoint ./api.js:1216088/372 ./toUnix.js:1216088/274 ./package.json:1216088/1108

--- api.js
+++ api.js
@@ -5,39 +5,29 @@ const app = express();
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
-
-// checkInt() => check if all the parameter inputs are integers
-// const checkInt = (par) => {
-//   console.log('in checkInt');
-//   for (var i = 0; i < par.length; i++) {
-//     if (!Number.isInteger(par[i])) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get("/:time", (req, res) => {
-  console.log('accessed');
-  let rawTime = req.params.time;
-  let convertedTime;
+  let time = req.params.time;
+  res.send(time);
+//   console.log('accessed');
+//   let rawTime = req.params.time;
+//   let convertedTime;
   
-  console.log(rawTime);
-  // isNum => check if all values are numeric string
-  let isNum = /^\d+$/.test(rawTime);
-  console.log(isNum);
+//   console.log(rawTime);
+//   // isNum => check if all values are numeric string
+//   let isNum = /^\d+$/.test(rawTime);
+//   console.log(isNum);
   
-  if (isNum) {
-    convertedTime = naturalTime(parseInt(rawTime));
-    res.send(convertedTime);
-  } else {
-    convertedTime = unixTime(rawTime);
-    res.send(rawTime);
-  }
+//   if (isNum) {
+//     convertedTime = naturalTime(parseInt(rawTime));
+//     res.send(convertedTime);
+//   } else {
+//     convertedTime = unixTime(rawTime);
+//     res.send(rawTime);
+//   }
   
   // res.send(typeof rawTime);
 });
--- toUnix.js
+++ toUnix.js
@@ -4,3 +4,14 @@ const unixTime = (naturalTime) => {
   let t = new Date(naturalTime);
   console.log('t: ' + t);
 }
+
+// checkInt() => check if all the parameter inputs are integers
+// const checkInt = (par) => {
+//   console.log('in checkInt');
+//   for (var i = 0; i < par.length; i++) {
+//     if (!Number.isInteger(par[i])) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
